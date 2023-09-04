545fe169b931ab407231c53c5b4923f4489e75c8
Ronaldo Ferreira <ronyferri@yahoo.com>
2023-09-04 10:16:53 -0300

    variaveis2.py

---
+++ variaveis2.py
@@ -0,0 +1,7 @@
+Python 3.10.12 (main, Jun 11 2023, 05:26:28) [GCC 11.4.0] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> nome= 'Ronaldo'
+>>> idade= 42
+>>> peso= 115
+>>> print(nome, idade, peso)
+Ronaldo 42 115
