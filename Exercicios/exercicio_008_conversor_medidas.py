251cc638ef6a7fe1cbb3cd365470df264da5a089
Ronaldo Ferreira <ronyferri@yahoo.com>
2023-09-08 10:42:20 -0300

    exercício_008_conversor_medidas

---
+++ Exercicios/exercicio_008_conversor_medidas.py
@@ -0,0 +1,7 @@
+#Escreva um programa que leia um valor em metros e exiba
+# convertido em centimetros e milimetros
+
+medida = float(input('Digite uma distância em metros: '))
+cm = medida * 100
+mm = medida * 1000
+print('A medida de {}m corresponde a {}cm e {}mm. '.format(medida, cm, mm))
