2db449c0197620a532e1a8f27e7f8129d1c18a59
Ronaldo Ferreira <ronyferri@yahoo.com>
2023-09-09 12:09:53 -0300

    Update exercício_015_aluguel_carro.py

--- Exercicios/exercício_015_aluguel_carro.py
+++ Exercicios/exercício_015_aluguel_carro.py
@@ -1,11 +1,12 @@
 #Escreva um programa qque pergunte a quantidade de KM
 #percorridos por um carro alugado e a quantidade de dias
 #pelos quais ele foi alugado. Calcule o preço a pagar, sabendo que, 
-#o carro custa R$60 por dia e R%0,15 por KM rodado
+#o carro custa R$60 por dia e R$ 0.15 por KM rodado
 
 
 d = int(input('Por quantos dias o carro foi alugado? '))
 km = float(input('Quantos kms o carro percorreu? '))
 v_d = d * 60 + km * 0.15
 print('O valor do aluguel é de R${:.2f}.'.format(v_d))
+
 
