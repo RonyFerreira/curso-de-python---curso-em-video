b7bcc219cff78e13c1113870856150e072467073
Ronaldo Ferreira <ronyferri@yahoo.com>
2023-09-09 12:29:17 -0300

    função_import.py

---
+++ aula_8/função_import.py
@@ -0,0 +1,15 @@
+import math
+num = int(input('Digite um número: '))
+raiz = math.sqrt(num)
+print('A raiz quadrada de {} é {:.2f}. '.format(num, raiz))
+
+
+#para arredondar o resultado para cima use:
+#.format(num, math.ceil(raiz)))
+
+#para arredondar para baixo use math.floor(raiz)
+
+
+
+
+      
