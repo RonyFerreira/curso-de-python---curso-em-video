11e4b03e363192b176cecbbbe8b34d59f17ed72a
Ronaldo Ferreira <ronyferri@yahoo.com>
2023-09-09 09:53:50 -0300

    Update exxercício_013_reajuste_salarial.py

--- Exercicios/exxercício_013_reajuste_salarial.py
+++ Exercicios/exxercício_013_reajuste_salarial.py
@@ -5,4 +5,4 @@
 
 salário = float(input('Qual é o seu salário? R$'))
 novo_salário = salário + (salário * 15 / 100)
-print('O seu salário de R${:.3f}, com o aumento de 15% passou a ser de R${:.3f}'.format(salário, novo_salário))
+print('O seu salário de R${:.2f}, com o aumento de 15% passou a ser de R${:.2f}'.format(salário, novo_salário))
