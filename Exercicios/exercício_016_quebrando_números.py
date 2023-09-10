5737d4b472ffef10f4ba01cb4d05e5c5846a6152
Ronaldo Ferreira <ronyferri@yahoo.com>
2023-09-09 22:43:43 -0300

    correct_import

--- Exercicios/exercício_016_quebrando_números.py
+++
@@ -1,3 +0,0 @@
-from math import floor
-num = float(input('Digite um valor: '))
-print('O valor digitado foi {} e seu valor inteiro é {} '.format(num, floor(num)))  
---
+++ Exercicios/exercício_016a_quebrando_números.py
@@ -0,0 +1,3 @@
+from math import trunc
+num = float(input('Digite um valor: '))
+print('O valor digitado foi {} e seu valor inteiro é {} '.format(num, trunc(num)))  
