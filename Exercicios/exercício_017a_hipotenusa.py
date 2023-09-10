dbcb4e9f6ab2a2b031633f4151dc73a2e2937ecf
Ronaldo Ferreira <ronyferri@yahoo.com>
2023-09-09 23:37:19 -0300

    hipotenusa

--- Exercicios/exercício_017a_hipotenusa.py
+++ Exercicios/exercício_017a_hipotenusa.py
@@ -2,3 +2,7 @@ from math import hypot
 cat_op = float(input('Digite o comprimento do cateto oposto: '))
 cat_adj = float(input('Digite o comprimento do cateto adjacente: '))
 print('O valor da hipotenusa é {:.2f} '.format(hypot(cat_op, cat_adj)))
+
+
+#ainda poderiamos ter criado uma variável "hi = hypot(cat_op, cat_adj)"
+#e no print colocar apenas ".format(hi)"
--- Exercicios/exercício_017b_hipotenusa.py
+++ Exercicios/exercício_017b_hipotenusa.py
@@ -1,7 +1,7 @@
 cat_op = float(input('Digite o commprimento do cateto oposto: '))
 cat_adj = float(input('Digite o comprimento do cateto adjacente: '))
 hip = (cat_op ** 2 + cat_adj ** 2) ** (1/2)
-print('A hipotenusa vai medir {} '.format(hip))
+print('A hipotenusa vai medir {:.2f} '.format(hip))
 
 
 
