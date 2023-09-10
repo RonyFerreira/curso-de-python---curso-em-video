eeb5c10ecec41380d860ea2b4c4999e0cfd18e07
Ronaldo Ferreira <ronyferri@yahoo.com>
2023-09-09 22:50:49 -0300

    Update exercício_016a_quebrando_números.py

--- Exercicios/exercício_016a_quebrando_números.py
+++ Exercicios/exercício_016a_quebrando_números.py
@@ -1,3 +1,7 @@
 from math import trunc
 num = float(input('Digite um valor: '))
-print('O valor digitado foi {} e seu valor inteiro é {} '.format(num, trunc(num)))  
+print('O valor digitado foi {} e seu valor inteiro é {} '.format(num, trunc(num))) 
+
+
+#para números inteiros poderiamos usar a função int
+#.format(num, int(num)) sem necessidade de importar nada
