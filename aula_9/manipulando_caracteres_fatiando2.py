82ae00adfc33411f89f47fd3c1d49257f15042ca
RonyFerreira <ronyferri@yahoo.com>
2023-09-12 12:53:04 -0300

    Update manipulando_caracteres_fatiando2.py

--- aula_9/manipulando_caracteres_fatiando2.py
+++ aula_9/manipulando_caracteres_fatiando2.py
@@ -3,3 +3,17 @@ print(frase[:13])
 
 
 #print(frase[13:]) faz o inverso do comando acima
+
+#print(frase[1:15]) irá mostrar os caracteres de 1 a 15, lembrando que os espaços no python se iniciam em zero - 'urso em video'
+
+
+#print(frase[1:15:2]) irá mostrar de 1 a 15 pulando de 2 em 2, 'us mVdo'
+
+#print(frase[1::2]) irá até o final pulando de 2 em 2, 'us mVdoPto'
+
+#print(frase[::2]) qquando não souber o início e nem o final coloque somente como exemplo qque o python irá pegar a string inteira pulando de 2 em 2
+
+
+
+
+
