0b2242cda081b73fe258027ef4ea29261edbb479
RonyFerreira <ronyferri@yahoo.com>
2023-09-13 10:51:23 -0300

    função split & dividido.py

---
+++ aula_9/função_split_dividido.py
@@ -0,0 +1,15 @@
+frase = 'Curso em Video Python'
+print (frase.split())
+
+
+#frase = 'Curso em Video Python'
+#dividido = frase.split()
+#print(dividido[0])
+
+# o código acima dividi a frase e o dividido mostra apenas o espaço de memória zero
+
+
+#frase = 'Curso em Video Python'
+#dividido = frase.split()
+#print(dividido[2][3])
+# o comando acima irá printar o segundo espaço de memória da frase e mostrar a terceira letra
