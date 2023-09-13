dd89cda1c20fc5a1af5a07779db0dd8a78532e3e
RonyFerreira <ronyferri@yahoo.com>
2023-09-13 10:24:16 -0300

    função len

---
+++ aula_9/função_len.py
@@ -0,0 +1,5 @@
+frase = 'Curso em Video Python'
+print(len(frase))
+
+
+#print(len(frase.strip())) essa função fará o python contar os espaços de caracteres subtraindo os espaços no início e no fim
--- aula_9/função_print+count.py
+++ aula_9/função_print+count.py
@@ -3,3 +3,4 @@ print(frase.count('o'))
 
 #esse comando fará o python contar as letras  fazendo distinção entre maiúsculas e minúsculas
 
+# print(frase.upper().count('O')) passa a frase para maiúsculo e conta quantas vezes tem a letra O 
