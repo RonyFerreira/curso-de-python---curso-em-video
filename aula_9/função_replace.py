ba319bb0fd8846360b77e38e8cee8767c87353bd
RonyFerreira <ronyferri@yahoo.com>
2023-09-13 10:32:22 -0300

    função_replace.py

---
+++ aula_9/função_replace.py
@@ -0,0 +1,8 @@
+frase = 'Curso em Video Python'
+print(frase.replace('Python', 'Android'))
+ 
+#esse comando substitui somente neste comando, no entanto a palavra python ainda é atribuida a frase. Observe o código a seguir:
+
+#frase = print(frase.replace('Python', 'Android')
+#print(frase)  nesse caso a palavra python foi substituida permanentemente
+
