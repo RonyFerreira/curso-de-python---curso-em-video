f35f55f4c4daf0fa57a2e3c21a003a9275fb5153
RonyFerreira <ronyferri@yahoo.com>
2023-09-14 12:51:50 -0300

    Update exercicio_022_analisando_nome.py

--- Exercicios/exercicio_022_analisando_nome.py
+++ Exercicios/exercicio_022_analisando_nome.py
@@ -6,3 +6,8 @@ print('Seu nome tem ao todo {} letras'.format(len(nome) - nome.count(' ')))
 #print('Seu primeiro nome tem ao todo {} letras '.format(nome.find(' ')))
 separa = nome.split()  
 print('Seu primeiro nome é {} e ele tem {} letras '.format(separa[0], len(separa[0])))
+
+#Exercício Python 22: Crie um programa que leia o nome completo de uma pessoa e mostre:
+#– O nome com todas as letras maiúsculas e minúsculas.
+#– Quantas letras ao todo (sem considerar espaços).
+#– Quantas letras tem o primeiro nome.
